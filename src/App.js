187c64a7226dd7fca47c6012c35b570ee34f088d
Tim Kmecl <tim.kmecl.tk@gmail.com>
2023-01-05 12:57:47 +0100

    refactoring table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import Tabela from './components/Tabela';
+
 const mesta = ['LJUBLJANA', 'PTUJ', 'MARIBOR', 'KOPER/CAPODISTRIA', 'ROGAŠKA SLATINA', 'JESENICE', 'PORTOROŽ/PORTOROSE', 'NOVO MESTO', 'ŠKOFJA LOKA', 'RAVNE NA KOROŠKEM', 'SLOVENSKE KONJICE', 'CELJE', 'TREBNJE', 'SLOVENJ GRADEC', 'RADOVLJICA', 'KRANJ', 'HOČE', 'NOVA GORICA', 'DOMŽALE', 'MURSKA SOBOTA', 'VELENJE', 'BLED', 'KRŠKO', 'IZOLA/ISOLA', 'ZAGORJE OB SAVI', 'TRBOVLJE', 'LOGATEC', 'BREŽICE']
 
 function App() {
   const [data, setData] = useState(null);
-  const [mesto, setMesto] = useState('LJUBLJANA');
+  const [city, setCity] = useState('LJUBLJANA');
   const [search, setSearch] = useState('');
 
   useEffect(() => {
@@ -16,33 +18,27 @@ function App() {
     });
   }, []);
 
-  const handleMestoChange = (e) => {
-    setMesto(e.target.value);
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
   }
 
   const handleSearchChange = (e) => {
     setSearch(e.target.value);
   }
 
-  let displayData = data && data.filter(item => item.city === mesto)
-  if (displayData && search) {
-    displayData = displayData.filter(item => item.lokal.toLowerCase().includes(search.toLowerCase()))
-  }
-  if (displayData) displayData.sort((a, b) => b.cena_diff_percent - a.cena_diff_percent)
-
   return (
     <div className="App">
       <div className='main'>
         <h1>Stanje študentske prehrane 2023</h1>
         <p>S 1. januarjem se je vrednost bona <em>zvišala</em> za <strong>0,68 €</strong>. 
-          Namesto ugodnejših cen za študente pa se je povprečno doplačilo <em>zvišalo</em> za <strong>1,08 €</strong> (<strong>35 %</strong>) </p>
+          Namesto ugodnejših cen za študente pa se je povprečno doplačilo <em>zvišalo</em> za <strong>1,08 € (35 %)</strong> </p>
         <p>Ob <strong>10,3 %</strong> letni inflaciji oz. <strong>18,6 %</strong> inflaciji cen hrane in pijače
           dobijo restavracije za obrok zdaj v povprečju <strong>30 %</strong> več kot lani.</p>
       </div>
       <div className='search'>
         <p>
           <label htmlFor="mesta">Izberi mesto: </label>
-          <select name="mesta" id="mesta" value={mesto} onChange={handleMestoChange} >
+          <select name="mesta" id="mesta" value={city} onChange={handleCityChange} >
             {mesta.map((item, index) => {
               return <option key={index} value={item}>{item}</option>
             })}
@@ -52,37 +48,17 @@ function App() {
           <label htmlFor="search">Išči restavracijo: </label>
           <input type="search" name="search" id="search" value={search} onChange={handleSearchChange}/>
         </p>
+        <p className='disclaimer'>
+            Podatki so informativne narave, ne jamčim za njihovo točnost. Interpretacijo prepuščam bralcu.
+        </p>
       </div>
-      <div className='table'>
-        <table>
-          <thead>
-            <tr>
-              <th>Restavracija</th>
-              <th>"Vrednost obroka"</th>
-              <th>Povišanje (%)</th>
-              <th>Povišanje</th>
-            </tr>
-          </thead>
-          <tbody>
-            {displayData && displayData.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td className='ime'>
-                    <a href={`https://www.studentska-prehrana.si/sl/restaurant/Details/${item.posid}`} target="_blank" rel="noreferrer">
-                      {item.lokal}
-                    </a>
-                  </td>
-                  <td>{item.cena.toFixed(2)} €</td>
-                  <td className={`procent ${item.cena_diff_percent > 18.6 ? 'red' : ''} ${(item.doplacilo === 0) ? 'free' : ''}`}>
-                    {item.cena_diff_percent.toFixed(0)} %
-                  </td>
-                  <td>{item.cena_diff.toFixed(2)} €</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
+      {
+        data && <Tabela data={data} city={city} search={search} />
+      }
+
+      <div id="created-by">
+		    <p>Website created by <a href="//kmecl.tk">Tim Kmecl</a></p>
+	    </div>
     </div>
   );
 }
---
+++ src/components/Tabela.js
@@ -0,0 +1,44 @@
+
+const Tabela = ({data, city, search}) => {
+
+	let displayData = data && data.filter(item => item.city === city)
+  if (displayData && search) {
+    displayData = displayData.filter(item => item.lokal.toLowerCase().includes(search.toLowerCase()))
+  }
+  if (displayData) displayData.sort((a, b) => b.cena_diff_percent - a.cena_diff_percent)
+
+	return (
+		<div className='table'>
+			<table>
+				<thead>
+					<tr>
+						<th>Restavracija</th>
+						<th>"Vrednost obroka"</th>
+						<th>Povišanje (%)</th>
+						<th>Povišanje</th>
+					</tr>
+				</thead>
+				<tbody>
+					{displayData && displayData.map((item, index) => {
+						return (
+							<tr key={index}>
+								<td className='ime'>
+									<a href={`https://www.studentska-prehrana.si/sl/restaurant/Details/${item.posid}`} target="_blank" rel="noreferrer">
+										{item.lokal}
+									</a>
+								</td>
+								<td>{item.cena.toFixed(2)} €</td>
+								<td className={`procent ${item.cena_diff_percent > 18.6 ? 'red' : ''} ${(item.doplacilo === 0) ? 'free' : ''}`}>
+									{item.cena_diff_percent.toFixed(0)} %
+								</td>
+								<td>{item.cena_diff.toFixed(2)} €</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		</div>
+	)
+}
+
+export default Tabela
